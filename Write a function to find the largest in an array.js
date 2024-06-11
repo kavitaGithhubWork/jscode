ac4dbc228f32848c6f93c79387a9a890af6f448e
innercoderKavita <34956887+kavitaGithhubWork@users.noreply.github.com>
2024-06-11 15:40:34 -0400

    Update Write a function to find the largest in an array.js

--- Write a function to find the largest in an array.js
+++ Write a function to find the largest in an array.js
@@ -1,1 +1,17 @@
 //Write a function to find the largest
+//Write a function to find the largest
+
+const arr = [1, 34, 66, 24, 78, 23]
+
+let largetnumber = arr[0]
+
+for (let i= 1 ; i< arr.length ; i ++){
+    if(arr[i]>largetnumber)
+        {
+            largetnumber = arr[i]
+        } 
+
+
+}
+
+console.log(largetnumber)
