b0052d0f14efa8296d87a9f05ad34f7b8540a00e
innercoderKavita <34956887+kavitaGithhubWork@users.noreply.github.com>
2024-06-11 15:25:44 -0400

    Update Write a function to sort an array of numbers.js

--- Write a function to sort an array of numbers.js
+++ Write a function to sort an array of numbers.js
@@ -1,1 +1,15 @@
 //Write a function to sort an array of numbers
+
+function sortNumber(numbers){
+
+    numbers.sort( function(a , b) {
+
+        return a-b;
+    } )
+
+    return numbers}
+
+// Example usage:
+const numbers = [3, 1, 8, 2, 5];
+const sortedNumbers = sortNumber(numbers);
+console.log("Sorted numbers:", sortedNumbers);
