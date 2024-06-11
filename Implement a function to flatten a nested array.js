c6fb4062740ad8e42cbbf35e36434465b3649fa3
innercoderKavita <34956887+kavitaGithhubWork@users.noreply.github.com>
2024-06-11 15:40:23 -0400

    Update Implement a function to flatten a nested array.js

---
+++ Implement a function to flatten a nested array.js
@@ -0,0 +1,16 @@
+//Write a function to find the largest
+
+const arr = [1, 34, 66, 24, 78, 23]
+
+let largetnumber = arr[0]
+
+for (let i= 1 ; i< arr.length ; i ++){
+    if(arr[i]>largetnumber)
+        {
+            largetnumber = arr[i]
+        } 
+
+
+}
+
+console.log(largetnumber)
