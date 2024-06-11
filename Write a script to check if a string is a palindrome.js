2cd78bf91731986c8a07bd6d940b882a4f4af76e
innercoderKavita <34956887+kavitaGithhubWork@users.noreply.github.com>
2024-06-11 14:39:45 -0400

    Update Write a script to check if a string is a palindrome.js

--- Write a script to check if a string is a palindrome.js
+++ Write a script to check if a string is a palindrome.js
@@ -1,1 +1,17 @@
 //Write a script to check if a string is a palindrome.
+
+const inputStr = "nitin"
+
+let newStr = inputStr.replace(/[^a-zA-Z0-9]/g,'')
+
+let reverStr = newStr.split('').reverse().join('')
+
+if(inputStr === reverStr){
+    console.log('true')
+    return true;
+    
+} else { 
+    console.log('true')
+    return false;
+    
+}
